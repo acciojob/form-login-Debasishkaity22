3f0012ad27cd697c08f831bc050c9c325aecd9c8
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-26 14:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function getFormvalue() {
     //Write your code here
-	form1.preventDefault();
 	const form1=document.getElementById("form1");
 	let first=form1.elements["fname"].value;
      let last=form1.elements["lname"].value;
 	alert(first+" "+last);
 	
 }
-// document.getElementById("form1").addEventListener('submit',function(ev){
-// ev.preventDefault();
-// 	getFormvalue();
-// })
+document.getElementById("form1").addEventListener('submit',function(ev){
+ev.preventDefault();
+	getFormvalue();
+})
